31a26d0150a4725fbaea35e845666316f4cddbd2
Ivan Tulinov <energiay@gmail.com>
2015-10-12 07:31:41 +0300

    Ручное редактирование и создание игроков

--- metanit_football/Controllers/HomeController.cs
+++ metanit_football/Controllers/HomeController.cs
@@ -18,6 +18,53 @@ namespace metanit_football.Controllers
             var players = db.Players.Include(p => p.Team);
             return View(players.ToList());
         }
-        
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Формируем селектЛист (список команд) для передачи в представление
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? Id)
+        {
+            if (Id==null)
+            {
+                return HttpNotFound();
+            }
+
+            Player player = db.Players.Find(Id);
+            if (player!=null)
+            {
+                SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
